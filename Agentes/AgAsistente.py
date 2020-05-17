588a695c7ea495416049e6f5e92cc8c9228d5440
laiaondono <laia.ondono@gmail.com>
2020-05-17 17:39:22 +0200

    .gitignore update

--- Agentes/AgAsistente.py
+++ Agentes/AgAsistente.py
@@ -20,11 +20,12 @@ from rdflib import URIRef,Namespace, Graph, FOAF
 from flask import Flask, request
 import rdflib
 
-from AgentUtil.FlaskServer import shutdown_server
-from AgentUtil.Agent import Agent
+
+# from AgentUtil.FlaskServer import shutdown_server
+# from AgentUtil.Agent import Agent
 __author__ = 'pau-laia-anna'
 
-"""
+
 # Configuration stuff
 hostname = socket.gethostname()
 port = 9011
@@ -115,26 +116,3 @@ if __name__ == '__main__':
     # Esperamos a que acaben los behaviors
     ab1.join()
     print('The End')
-"""
-g = Graph()
- 
-g.parse('C:/Users/pauca/Desktop/ECSDI/Ontologia.owl', format='xml')
-name = Namespace('C:/Users/pauca/Desktop/ECSDI/Ontologia.owl')
-#print(g.triples(None,FOAF.name,"hasdh"))
-"""
-for s, p, o in g:
-    if (str(s) == "http://www.semanticweb.org/pauca/ontologies/2020/3/untitled-ontology-4#Producto_5PLUYF"):
-        print(str(p) + " : " +str(o))
-    #print("Sujeto " + str(s))
-    #print("Predicado " + str(p))
-    #print("Objeto " + str(o))
-"""
-node = URIRef('http://mundo.mundial.org/persona/pedro')
-
-res = g.query(""" PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-                    SELECT DISTINCT ?a ?Nombre
-                    WHERE {
-                        ?a foaf:age ?PrecioProducto .
-                        ?a foaf:name ?Nombre .
-                        FILTER {?PrecioProducto > 18}
-                        }""")
