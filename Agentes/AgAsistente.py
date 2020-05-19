f88c45c16d14af0845618fdc44d134758615c00a
laiaondono <laia.ondono@gmail.com>
2020-05-19 11:51:13 +0200

    changes AgAsistente.py

--- Agentes/AgAsistente.py
+++ Agentes/AgAsistente.py
@@ -12,19 +12,17 @@ Asume que el agente de registro esta en el puerto 9000
 
 @author: pau-laia-anna
 """
-from imaplib import Literal
+
 from multiprocessing import Process, Queue
 import socket
 
-from rdflib import URIRef, Namespace, Graph, FOAF, RDF
+from rdflib import Namespace, Graph, RDF, Literal, URIRef
 from flask import Flask, request, render_template
-import rdflib
 
-from Agentes.SimpleDirectoryService import register_agent
 from Util.ACLMessages import build_message, send_message
 from Util.FlaskServer import shutdown_server
 from Util.Agent import Agent
-from Util.OntoNamespaces import DSO, ONTO, ACL
+from Util.OntoNamespaces import ONTO, ACL
 
 __author__ = 'pau-laia-anna'
 
@@ -53,8 +51,8 @@ DirectoryAgent = Agent('DirectoryAgent',
 
 AgBuscadorProductos = Agent('AgBuscadorProductos',
                             agn.AgBuscadorProductos,
-                            'http://%s:9000/Search' % hostname,
-                            'http://%s:9000/Stop' % hostname)
+                            'http://%s:9010/Search' % hostname,
+                            'http://%s:9010/Stop' % hostname)
 
 # Global triplestore graph
 dsgraph = Graph()
@@ -62,8 +60,8 @@ dsgraph = Graph()
 cola1 = Queue()
 
 resultats = []
-# Flask stuff
 
+# Flask stuff
 app = Flask(__name__, template_folder='../templates')
 
 
@@ -110,69 +108,65 @@ def search_products():
     if request.method == 'GET':
         return render_template('search_products.html', products=None)
     else:
-        if request.form['submit'] == 'Buscar':
+        if request.form['submit'] == 'Busca':
             name = request.form['name']
-            price = request.form['price']
+            minPrice = request.form['maxPrice']
+            maxPrice = request.form['maxPrice']
             brand = request.form['brand']
-
             global mss_cnt
             g = Graph()
 
             action = ONTO['BuscarProductos_' + str(mss_cnt)]
-            g.add(action, RDF.type, ONTO.BuscarProductos)
+            g.add((action, RDF.type, ONTO.BuscarProductos))
 
             if name:
                 nameRestriction = ONTO['RestriccionNombre_' + str(mss_cnt)]
-                g.add(nameRestriction, RDF.type, ONTO.RestriccionNombre)
-                g.add(nameRestriction, ONTO.Nombre, Literal(name)) # datatype=XSD.string !??!!?!?!?
+                g.add((nameRestriction, RDF.type, ONTO.RestriccionNombre))
+                g.add((nameRestriction, ONTO.Nombre, Literal(name))) # datatype=XSD.string !??!!?!?!?
+                g.add((action, ONTO.RestringidaPor, URIRef(nameRestriction)))
 
-            if price:
-                priceRestriction = ONTO['RestriccionPrecio_' + str(mss_cnt)]
-                g.add(priceRestriction, RDF.type, ONTO.RestriccionPrecio)
-                g.add(priceRestriction, ONTO.PrecioProducto, Literal(price)) # datatype=XSD.string !??!!?!?!?
+            if minPrice:
+                minPriceRestriction = ONTO['RestriccionPrecio_' + str(mss_cnt)]
+                g.add((minPriceRestriction, RDF.type, ONTO.RestriccionPrecio))
+                g.add((minPriceRestriction, ONTO.PrecioMinimo, Literal(minPrice)))
+                g.add((action, ONTO.RestringidaPor, URIRef(minPriceRestriction)))
+
+            if maxPrice:
+                maxPriceRestriction = ONTO['RestriccionPrecio_' + str(mss_cnt)]
+                g.add((maxPriceRestriction, RDF.type, ONTO.RestriccionPrecio))
+                g.add((maxPriceRestriction, ONTO.PrecioMaximo, Literal(maxPrice)))
+                g.add((action, ONTO.RestringidaPor, URIRef(maxPriceRestriction)))
 
             if brand:
                 brandRestriction = ONTO['RestriccionMarca_' + str(mss_cnt)]
-                g.add(brandRestriction, RDF.type, ONTO.RestriccionMarca)
-                g.add(brandRestriction, ONTO.Marca, Literal(brand)) # datatype=XSD.string !??!!?!?!?
+                g.add((brandRestriction, RDF.type, ONTO.RestriccionMarca))
+                g.add((brandRestriction, ONTO.Marca, Literal(brand))) # datatype=XSD.string !??!!?!?!?
+                g.add((action, ONTO.RestringidaPor, URIRef(brandRestriction)))
 
             msg = build_message(g, ACL.request, AgAsistente.uri, AgBuscadorProductos.uri, action, mss_cnt)
             mss_cnt += 1
             gproducts = send_message(msg, AgBuscadorProductos.address)
-
+            """
             products_list = []
-            for s, p, o in gproducts:
-                """ NOSTRE
+            product = {}
+            product['name'] = "nomproductttt"
+            product['brand'] = "nbrandroductttt"
+            product['price'] = 33
+            products_list.append(product)
+            """
+            products_list = []
+            product = {}
+            for s, o in gproducts:
                 product['url'] = s
-                product['name'] =
-                product['price'] =
-                product['brand'] =
-
+                product['name'] = o
+                product['brand'] = o
+                product['price'] = o
                 products_list.append(product)
-                """
-
-                """ GITHUB
-                if s not in subject_pos:
-                    subject_pos[s] = index
-                    products_list.append({})
-                    index += 1
-                if s in subject_pos:
-                    subject_dict = products_list[subject_pos[s]]
-                    if p == RDF.type:
-                        subject_dict['url'] = s
-                    elif p == ONTO.Nombre:
-                        subject_dict['name'] = o
-                    elif p == ONTO.PrecioProducto:
-                        subject_dict['price'] = o
-                    elif p == ONTO.Marca:
-                        subject_dict['brand'] = o
-                    products_list[subject_pos[s]] = subject_dict
-                    """
 
             return render_template('search_products.html', products=products_list)
 
 
-def agentbehavior1():
+def agentbehavior1(queue):
     """
     Un comportamiento del agente
 
